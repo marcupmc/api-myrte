9ef0646e13e4b133fbf1298ccb02d6160114af7b
Marc Gregoire <marc.gregoire@tinyclues.com>
2017-01-15 19:08:31 +0100

    add mock route for products

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    res.send('Hello World!')
+});
+
+app.get('/products', function (req, res) {
+    var products = [
+        {
+            id: 1,
+            name: 'Incroyable myrte',
+            description: 'Melange parfait de myrte et de rhum',
+            image: 'to/define',
+        },
+        {
+            id: 2,
+            name: 'Parfait citron',
+            description: 'Rhum infusé au citron',
+            image: 'to/define',
+        }
+    ]
+    res.send(products);
+});
 
 app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 5000!')
 })
