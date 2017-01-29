1dcb69b9ffc6f1a1e430f259324b0830704ea92d
Marc Gregoire <marc.gregoire@tinyclues.com>
2017-01-29 16:50:51 +0100

    cors again

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express()
-
+const cors = require('cors');
+app.options('*', cors());
+ 
 app.get('/products', (req, res) => {
     const MongoClient = require('mongodb').MongoClient;
     const co = require('co');
     const mongodb_uri = process.env.MONGODB_URI;
-    res.header('Access-Control-Allow-Origin', '*');
     co(function*() {
         var db = yield MongoClient.connect(mongodb_uri);
         console.log("Connected correctly to server");
         db.collection('products').find().toArray((err, results) => {
+            res.header('Access-Control-Allow-Origin', '*');
             res.send(results);
         });
         db.close();
